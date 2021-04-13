c0cf1222595da4ce5b62c21e452491c36f48c382
IceD0G <34693762+IceD0G@users.noreply.github.com>
2021-04-13 19:45:57 +0300

    weather-app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import axios from 'axios';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [weather, setWeather] = useState(null);
+	const [input, setInput] = useState('');
+	useEffect(() => {
+		axios
+			.get(
+				'http://api.weatherapi.com/v1/current.json?key=a4ab2c97c17d41b8a99154701211304&q=London',
+			)
+			.then(data => {
+				setWeather(data.data);
+				console.log(data.data);
+			})
+			.catch(err => console.log(err));
+	}, []);
+	//event
+	const weatheInput = e => {
+		setInput(e.target.value);
+	};
+	const searchWeather = () => {
+		axios
+			.get(
+				`http://api.weatherapi.com/v1/current.json?key=a4ab2c97c17d41b8a99154701211304&q=${input}`,
+			)
+			.then(data => {
+				setWeather(data.data);
+			})
+			.catch(err => console.log(err));
+	};
+	return (
+		<div>
+			{weather && (
+				<div>
+					<input type='text' onChange={weatheInput} />
+					<button onClick={searchWeather}>Search city</button>
+					<div className='weather-info'>
+						<h1>{weather.location.country}</h1>
+						<h2>{weather.location.name}</h2>
+						<h2>{weather.location.region}</h2>
+						<h4>{weather.current.condition.text}</h4>
+						<img src={weather.current.condition.icon} alt='' />
+						<h4>Weather is {weather.current.temp_c} celsius</h4>
+					</div>
+				</div>
+			)}
+		</div>
+	);
 }
 
 export default App;
